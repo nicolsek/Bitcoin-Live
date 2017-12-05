b260cdc7d86bebeea90992338fa9ba3c737ea64a
GeekishGirl <tusek.nicole@gmail.com>
2017-12-04 16:13:24 -0800

    Added basis for live updating currency.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var table = $('#table'); //The market val thing.
+var marketval = $('#market_val');
 
+function update(first) {
+    let now = new Date();
+
+    table.find("tr:gt(0)").remove();
+
+    let resp = $.getJSON("https://blockchain.info/ticker", function(data) {
+        $.each(data, function(key, val) {
+            var rowStart = "<tr>"
+            var rowContents = ("<td>" + val.symbol + " - " + key + "</td>" + "<td>" + val.sell + "</td>");
+            var rowEnd = "</tr>"
+
+            var rowFull = rowStart + rowContents + rowEnd;
+            marketval.append(rowFull);
+        })
+    }); //Response from conversion stuff.
+}
