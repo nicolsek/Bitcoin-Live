0b5a7f146127e2414de1e4f2be93b6f3ea274ff7
GeekishGirl <tusek.nicole@gmail.com>
2017-12-04 18:31:25 -0800

    Added README and more info!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-var table = $('#table'); //The market val thing.
-var marketval = $('#market_val');
+var pastVals = [];
 
 function update(first) {
     let now = new Date();
 
     $(".remove").remove(); //Remove all children.
 
-    let resp = $.getJSON("https://blockchain.info/ticker", function(data) {
+    var resp = $.getJSON("https://blockchain.info/ticker", function(data) {
+        var table = $('#table'); //The market val thing.
+        var marketval = $('#market_val');        
         var it = 0;
 
         $.each(data, function(key, val) {
@@ -21,14 +22,44 @@ function update(first) {
             }
             
             if (!first) {
-                var rowContents = ("<td id='cell" + it + "'>" + "<i class='fa fa-btc' aria-hidden='true'></i> " + val.sell + "</td>");                
+                let tail = "";
+                let change = ((val.sell - pastVals[it]) / pastVals[it]) * 100;
+
+                if (change < 0) {
+                    tail = "<p style='color : red'>" + change.toFixed(3) + "%</p>"
+                }
+
+                if (change > 0) {
+                    tail = "<p style='color : green'>" + "+" + change.toFixed(3) + "%</p>"                    
+                }
+
+                if (change == 0){
+                    tail = "<p style='color : grey'>" + "+" + 0 + "%</p>"                                        
+                }
+                
+                var rowContents = ("<td id='cell" + it + "'>" + "<i class='fa fa-btc' aria-hidden='true'></i> " + val.sell + tail + "</td>");                
                 $("#cell" + it).replaceWith(rowContents);
             }
 
+            pastVals[it] = val.sell;
+
             it++;
-        })
+        });
+
     }); //Response from conversion stuff.
+
+    var resp = $.getJSON("https://api.blockchain.info/stats", function(data) {
+        var json = resp.responseJSON;
+        
+        var mined = $('#mined'); //# of Bitcoin mined.
+        var revenue = $("#revenue"); //Amount of revenue in USD.
+        var usdval = $("#usdval");
+        var transaction = $("#transaction");
+
+        mined.text(json.n_btc_mined);
+        revenue.text(json.miners_revenue_usd);
+    }); //Response from stats;
 }
 
 
-setInterval(update, 5000);
+setInterval(update, 1000 * 30);
